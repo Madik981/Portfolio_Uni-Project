2cbc428cc9b35a820000b2f0b8d293735cf2bc0b
Madik981 <madi12312456@gmail.com>
2024-12-26 20:48:28 +0500

    implemented mobile hamburger menu in header

---
+++ script.js
@@ -0,0 +1,7 @@
+const toggleMenu = () => {
+    const menu = document.querySelector(".mobile-nav-links");
+    const icon = document.querySelector(".mobile-nav-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+  
